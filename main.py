c9a7ab312e7c740d013e2408ed6e4976299f5be9
aonsafdarirl <35302199-aonsafdarirl@users.noreply.replit.com>
2026-01-31 12:55:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from random import choice
+while True:
+    print('\n--- Utility Toolkit ---')
+    print('1) Add two numbers')
+    print('2) Even or Odd')
+    print('3) Celsius to Fahrenheit')
+    print('4) Roll a die')
+    print('5) Exit')
+
+    choice = input('Choose: ')
+    if choice == '1':
+        
