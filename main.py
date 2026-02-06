15fbed09c46cd04d08e68d90862050fe116f1030
Aon Safdar <91821844+aonsafdar@users.noreply.github.com>
2026-02-06 21:44:55 +0000

    classObjects

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
-while True:
-    print('\n--- Utility Toolkit ---')
-    print('1) Add two numbers')
-    print('2) Even or Odd')
-    print('3) Celsius to Fahrenheit')
-    print('4) Roll a die')
-    print('5) Exit')
+# create classes
+class human:
 
-    choice = input('Choose: ')
-    if choice == '1':
-        break
-    elif choice == '2':
-        break
-    elif choice == '3':
-        break
-    elif choice == '4':
-        break
+    def __init__(self, name, age):  # constructor
+        self.name = name
+        self.age = age
+        print("I am a human")
+        print("My name is", self.name)
+        print("My age is", self.age)
 
-print("exited")
-print("\n")
-print("Thank you for using the Utility Toolkit!")
+
+class man(human):  # inheritance
+
+    def __init__(self, name, age, gender):  # constructor
+        super().__init__(name, age)  # call parent constructor
+        self.gender = gender
+        print("I am a man")
+        print("My gender is", self.gender)
+
+
+class woman(human):  # inheritance
+
+    def __init__(self, name, age, gender):
+        super().__init__(name, age)
+        self.gender = gender
+        print("I am a woman")
+        print("My gender is", self.gender)
+
+
+class child(human):  # inheritance
+
+    def __init__(self, name, age, gender):
+        super().__init__(name, age)
+        self.gender = gender
+        print("I am a child")
+        print("My gender is", self.gender)
+
+
+# create objects
+human1 = human("John", 30)
+man1 = man("John", 30, "male")
+woman1 = woman("Jane", 25, "female")
+child1 = child("Jack", 5, "male")
+# print objects
+print(human1)
+print(man1)
+print(woman1)
+print(child1)
+# print attributes
+print(human1.name)
+print(human1.age)
